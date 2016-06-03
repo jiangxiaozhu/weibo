235cd9ae1f109b63704899e99ec91d1ea6b36ff6
hongtu <qiqiang.jiang@chemayi.com>
2016-06-03 14:22:12 +0800

    commit up

---
+++ Application/Home/Controller/LoginController.class.php
@@ -0,0 +1,18 @@
+<?php
+namespace Home\Controller;
+
+use Think\Controller;
+
+
+class LoginController extends Controller
+{
+
+    /**
+     * login µÇÂ½Ò³Ãæ
+     */
+    public function login(){
+        $this->display();
+    }
+}
+
+
---
+++ Application/Home/Controller/RegisterController.class.php
@@ -0,0 +1,15 @@
+<?php
+namespace Home\Controller;
+
+use Think\Controller;
+
+class RegisterController extends Controller
+{
+    /**
+     * ×¢²áÒ³Ãæ
+     */
+    public function register()
+    {
+        $this->display();
+    }
+}
